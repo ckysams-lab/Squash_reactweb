fc5686ddb0c3b9d7e38d605774ce36aea3cfa146
ckysams-lab <ckysams@bcklas.edu.hk>
2026-02-26 19:46:26 +0800

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+// .eslintrc.cjs
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier', // 關鍵！與 Prettier 整合
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
