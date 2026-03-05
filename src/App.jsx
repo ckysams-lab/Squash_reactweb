1ce021331c45457b4b66e589ec86e6194aaf4614
ckysams-lab <ckysams@bcklas.edu.hk>
2026-03-05 17:40:07 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2263,7 +2263,7 @@ const myDashboardData = useMemo(() => {
                     onClick={() => deleteItem('awards', award.id)}
                     className="absolute top-4 right-4 p-2 bg-black/20 backdrop-blur text-white/70 hover:text-red-500 hover:bg-white rounded-full transition-all opacity-0 group-hover:opacity-100 z-30"
                   >
-                    <Usercog size={16}/>
+                    <UserCog size={16}/>
                   </button>
                )}
           </div>
