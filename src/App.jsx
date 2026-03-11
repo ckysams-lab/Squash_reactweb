e8fb381cf0ccf110faa49d797d97372c6f79eb90
ckysams-lab <ckysams@bcklas.edu.hk>
2026-03-11 09:24:20 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ const LiveScoreboardDisplay = ({ liveMatches, TrophyIcon }) => {
 };
 
 export default function App() {
-  const { students, competitions, monthlyStars, leagueMatches } = useFirebaseData();
+  const { students, competitions, monthlyStars, leagueMatches } = useFirebaseData(user);
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
   const [currentUserInfo, setCurrentUserInfo] = useState(null);
