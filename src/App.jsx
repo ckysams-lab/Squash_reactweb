9dd5c23b185d3d56e07cf343c0ac3eed8276ba2c
ckysams-lab <ckysams@bcklas.edu.hk>
2026-03-14 23:14:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ import CreatePostModal from './components/CreatePostModal';
 import ExternalMatchesPage from './pages/ExternalMatchesPage';
 import SocialFeedPage from './pages/SocialFeedPage';
 import RankingPage from './pages/RankingPage';
-import VideoAnalysisPage from './pages/VideoAnalysisPage';
 import { toDataURL, getAcademicYear, readCSVFile, compressImage, getYouTubeEmbedUrl } from './utils/helpers';
 import { useFirebaseData } from './hooks/useFirebaseData';
 import LiveScoreboardDisplay from './components/LiveScoreboardDisplay';
