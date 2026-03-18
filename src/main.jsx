8ccf1095f883fb0149a3f27d38c64cc23498009c
ckysams-lab <ckysams@bcklas.edu.hk>
2026-03-18 22:35:34 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // 確保這一行存在！
+// 檔案：src/main.jsx 的最頂部
+
+// 👇 加入這一行，告訴 Vite 要掛載 PWA 的註冊腳本 👇
+import 'virtual:pwa-register';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
